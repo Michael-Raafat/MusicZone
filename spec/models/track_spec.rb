4b4d18ca1765eacaab45b4f1ae34136bfa1e3056
Amr Mohamed Nasr <re13amr@gmail.com>
2019-04-12 22:49:16 +0200

    fix tests

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -17,11 +17,6 @@ RSpec.describe Track, type: :model do
 	  	expect(subject).to_not be_valid
 	  end
 	  
-	  it "is not valid without an audio" do
-	  	subject.audio = nil
-	  	expect(subject).to_not be_valid
-	  end
-	  
 	  it { should validate_uniqueness_of(:title) }
   end
 
