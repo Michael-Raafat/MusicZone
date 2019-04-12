0443eca134ede3799f9001ae18c5fe80a5a14968
Amr Mohamed Nasr <re13amr@gmail.com>
2019-04-12 20:10:37 +0200

    clean migrations, adjust models

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,7 +2,7 @@ class Track < ActiveRecord::Base
   belongs_to :admin_user
   has_many :track_tags
   has_many :tags, :through => :track_tags
-  validates_presence_of :title, :admin_user
+  validates_presence_of :title, :admin_user, :audio
   validates_uniqueness_of :title
   mount_uploader :audio, AudioUploader
 end
--- db/migrate/20190412164323_add_audio_to_tracks.rb
+++
@@ -1,5 +0,0 @@
-class AddAudioToTracks < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tracks, :audio, :string
-  end
-end
