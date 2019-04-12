502583108369e8b302a4907e8fd66f7e556faf45
Marc Magdi <mavloper@gmail.com>
2019-04-12 23:19:35 +0200

    Add Faker

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,7 +32,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
 
-  config.infer_spec_type_from_file_location!  
+  config.infer_spec_type_from_file_location!
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
