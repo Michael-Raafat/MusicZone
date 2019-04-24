619976b8b85c593501092b8b7b4d1ac87eccc7e3
Michael <micoraafat96@gmail.com>
2019-04-24 23:05:19 +0200

    showing tracks depending on tags

--- app/controllers/track_listener_controller.rb
+++ app/controllers/track_listener_controller.rb
@@ -1,6 +1,9 @@
 class TrackListenerController < ApplicationController
   def index
     @user = current_user
-    @tracks = Track.all
+    @tags = @user.user_tags
+    @trackTag = TrackTag.where(id: @tags.ids)
+    @track_ids = @trackTag.select(:track_id)
+    @tracks = Track.where(id: @track_ids)
   end
 end
