cc5b1c7e1087750b938a8be2be63b0f02d1bd6d7
Michael <micoraafat96@gmail.com>
2019-04-25 12:57:33 +0200

    fixing tacks with tags

--- app/controllers/track_listener_controller.rb
+++ app/controllers/track_listener_controller.rb
@@ -1,9 +1,9 @@
 class TrackListenerController < ApplicationController
   def index
     @user = current_user
-    @tags = @user.user_tags
-    @trackTag = TrackTag.where(id: @tags.ids)
-    @track_ids = @trackTag.select(:track_id)
-    @tracks = Track.where(id: @track_ids)
+    tags = @user.user_tags
+    trackTag = TrackTag.where(tag_id: tags.ids)
+    track_ids = trackTag.select(:track_id)
+    @tracks = Track.where(id: track_ids)
   end
 end
