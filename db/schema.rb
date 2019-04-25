992ab696fbd6ebee86e25aab79e2ff6c5c8c79cc
Michael <micoraafat96@gmail.com>
2019-04-26 00:09:04 +0200

    trying to add tests

--- db/schema.rb
+++ db/schema.rb
@@ -64,9 +64,9 @@ ActiveRecord::Schema.define(version: 2019_04_25_170440) do
   create_table "tracks", force: :cascade do |t|
     t.string "title"
     t.bigint "admin_user_id"
-    t.string "audio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "audio"
     t.index ["admin_user_id"], name: "index_tracks_on_admin_user_id"
   end
 
--- spec/controllers/track_listener_controller_spec.rb
+++ spec/controllers/track_listener_controller_spec.rb
@@ -1,12 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe TrackListenerController, type: :controller do
-
+  include Devise::TestHelpers
+  let(:user) { FactoryBot.create(:user) }
   describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "redirect if not logged in" do
+      sign_out user
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+    it "show tracks of user" do
+      new_tracks = FactoryBot.create_list(:track, 4)
+      new_tags = FactoryBot.create_list(:tag, 6)
+      new_trackTags = FactoryBot.create_list(:track_tag, 5)
+      new_trackTags[1].track_id = 1
+      new_trackTags[1].tag_id = 1
+      new_trackTags[2].track_id = 3
+      new_trackTags[2].tag_id = 1
+      new_trackTags[3].track_id = 2
+      new_trackTags[3].tag_id = 2
+      new_tracks[1].tags = [new_tags[1]]
+      new_tracks[2].tags = [new_tags[2]]
+      new_tracks[3].tags = [new_tags[1]]
+      user.tags = [new_tags[1], new_tags[3], new_tags[5]]
+      user.save
+      get :index
+      expect(assigns(:tracks)).to match_array([new_tracks[1], new_tracks[3]])
+    end
+
   end
 
 end
