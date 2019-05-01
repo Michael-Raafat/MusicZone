7d1865825bf557f99b13eafe6c451ae0d4772deb
Amr Mohamed Nasr <re13amr@gmail.com>
2019-05-01 03:37:00 +0200

    add more tests

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -7,23 +7,47 @@ def login_via_form(email, password)
   click_button "Sign in"
 end
 
-describe "User operations", type: :feature do
+describe "User registration", type: :feature do
   let!(:user) { FactoryBot.create(:user) }
   let(:valid_attributes) { FactoryBot.attributes_for(:user) }
 
-  it "registers a new user" do
+  it "register without providing any field" do
     visit "/"
     click_link "Sign up"
-    fill_in "Email", with: valid_attributes[:email]
+    fill_in "Email", with: nil
+    fill_in "Password", with: nil
+    fill_in "Confirm Password", with: nil
+    fill_in "First Name", with: nil
+    fill_in "Last Name", with: nil
+    click_button "Sign up"
+    expect(page).to have_content "errors prohibited this user from being saved"
+  end
+
+  it "register a user without providing an email" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: nil
     fill_in "Password", with: valid_attributes[:password]
     fill_in "Confirm Password", with: valid_attributes[:password]
     fill_in "First Name", with: valid_attributes[:first_name]
     fill_in "Last Name", with: valid_attributes[:last_name]
     click_button "Sign up"
-    expect(page).to have_content "Logged in as #{valid_attributes[:email]}"
+    expect(page).to have_content "error prohibited this user from being saved"
   end
 
-  it "registers a new user without first name" do
+  it "register a user without providing a password" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: valid_attributes[:email]
+    fill_in "Password", with: nil
+    fill_in "Confirm Password", with: valid_attributes[:password]
+    fill_in "First Name", with: valid_attributes[:first_name]
+    fill_in "Last Name", with: valid_attributes[:last_name]
+    click_button "Sign up"
+    expect(page).to have_content "errors prohibited this user from being saved"
+  end
+
+  it "register a user without providing a first name" do
     visit "/"
     click_link "Sign up"
     fill_in "Email", with: valid_attributes[:email]
@@ -35,7 +59,31 @@ describe "User operations", type: :feature do
     expect(page).to have_content "error prohibited this user from being saved"
   end
 
-  it "registers two users with same credentials" do
+  it "register a user without providing a last name" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: valid_attributes[:email]
+    fill_in "Password", with: valid_attributes[:password]
+    fill_in "Confirm Password", with: valid_attributes[:password]
+    fill_in "First Name", with: valid_attributes[:first_name]
+    fill_in "Last Name", with: nil
+    click_button "Sign up"
+    expect(page).to have_content "error prohibited this user from being saved"
+  end
+
+  it "register a new user with invalid email format" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: valid_attributes[:first_name]
+    fill_in "Password", with: valid_attributes[:password]
+    fill_in "Confirm Password", with: valid_attributes[:password]
+    fill_in "First Name", with: valid_attributes[:first_name]
+    fill_in "Last Name", with: valid_attributes[:last_name]
+    click_button "Sign up"
+    expect(page).to have_content "error prohibited this user from being saved"
+  end
+
+  it "register two users with same credentials" do
     visit "/"
     click_link "Sign up"
     fill_in "Email", with: valid_attributes[:email]
@@ -57,10 +105,116 @@ describe "User operations", type: :feature do
     expect(page).to have_content "error prohibited this user from being saved"
   end
 
+  it "register two users with same credentials with different emails" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: valid_attributes[:email]
+    fill_in "Password", with: valid_attributes[:password]
+    fill_in "Confirm Password", with: valid_attributes[:password]
+    fill_in "First Name", with: valid_attributes[:first_name]
+    fill_in "Last Name", with: valid_attributes[:last_name]
+    click_button "Sign up"
+    expect(page).to have_content "Logged in as #{valid_attributes[:email]}"
+    click_link("Logout", match: :first)
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: "uni_" + valid_attributes[:email]
+    fill_in "Password", with: valid_attributes[:password]
+    fill_in "Confirm Password", with: valid_attributes[:password]
+    fill_in "First Name", with: valid_attributes[:first_name]
+    fill_in "Last Name", with: valid_attributes[:last_name]
+    click_button "Sign up"
+    expect(page).to have_content "Logged in as uni_#{valid_attributes[:email]}"
+  end
+  
+  it "register a new user with non-matching passwords" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: valid_attributes[:email]
+    fill_in "Password", with: valid_attributes[:password]
+    fill_in "Confirm Password", with: "a" + valid_attributes[:password]
+    fill_in "First Name", with: valid_attributes[:first_name]
+    fill_in "Last Name", with: valid_attributes[:last_name]
+    click_button "Sign up"
+    expect(page).to have_content "error prohibited this user from being saved"
+  end
+
+  it "register a new user" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: valid_attributes[:email]
+    fill_in "Password", with: valid_attributes[:password]
+    fill_in "Confirm Password", with: valid_attributes[:password]
+    fill_in "First Name", with: valid_attributes[:first_name]
+    fill_in "Last Name", with: valid_attributes[:last_name]
+    click_button "Sign up"
+    expect(page).to have_content "Logged in as #{valid_attributes[:email]}"
+  end
+
+end
+
+describe "User login", type: :feature do
+  let!(:user) { FactoryBot.create(:user) }
+  let(:valid_attributes) { FactoryBot.attributes_for(:user) }
+
   it "log in unregistered user" do
     visit "/"
     fill_in "Email", with: valid_attributes[:email]
     fill_in "Password", with: valid_attributes[:password]
+    click_button "Log in"
+    expect(page).to_not have_content "Logged in as #{valid_attributes[:email]}"
+  end
+
+  it "log in without email and password" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: valid_attributes[:email]
+    fill_in "Password", with: valid_attributes[:password]
+    fill_in "Confirm Password", with: valid_attributes[:password]
+    fill_in "First Name", with: valid_attributes[:first_name]
+    fill_in "Last Name", with: valid_attributes[:last_name]
+    click_button "Sign up"
+    expect(page).to have_content "Logged in as #{valid_attributes[:email]}"
+    click_link("Logout", match: :first)
+    visit "/"
+    fill_in "Email", with: nil
+    fill_in "Password", with: nil
+    click_button "Log in"
+    expect(page).to_not have_content "Logged in as #{valid_attributes[:email]}"
+  end
+
+  it "log in without email" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: valid_attributes[:email]
+    fill_in "Password", with: valid_attributes[:password]
+    fill_in "Confirm Password", with: valid_attributes[:password]
+    fill_in "First Name", with: valid_attributes[:first_name]
+    fill_in "Last Name", with: valid_attributes[:last_name]
+    click_button "Sign up"
+    expect(page).to have_content "Logged in as #{valid_attributes[:email]}"
+    click_link("Logout", match: :first)
+    visit "/"
+    fill_in "Email", with: nil
+    fill_in "Password", with: valid_attributes[:password]
+    click_button "Log in"
+    expect(page).to_not have_content "Logged in as #{valid_attributes[:email]}"
+  end
+
+  it "log in without password" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: valid_attributes[:email]
+    fill_in "Password", with: valid_attributes[:password]
+    fill_in "Confirm Password", with: valid_attributes[:password]
+    fill_in "First Name", with: valid_attributes[:first_name]
+    fill_in "Last Name", with: valid_attributes[:last_name]
+    click_button "Sign up"
+    expect(page).to have_content "Logged in as #{valid_attributes[:email]}"
+    click_link("Logout", match: :first)
+    visit "/"
+    fill_in "Email", with: valid_attributes[:email]
+    fill_in "Password", with: nil
     click_button "Log in"
     expect(page).to_not have_content "Logged in as #{valid_attributes[:email]}"
   end
@@ -81,5 +235,23 @@ describe "User operations", type: :feature do
     fill_in "Password", with: valid_attributes[:password]
     click_button "Log in"
     expect(page).to have_content "Logged in as #{valid_attributes[:email]}"
+  end
+
+  it "log in registered user with wrong password" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: valid_attributes[:email]
+    fill_in "Password", with: valid_attributes[:password]
+    fill_in "Confirm Password", with: valid_attributes[:password]
+    fill_in "First Name", with: valid_attributes[:first_name]
+    fill_in "Last Name", with: valid_attributes[:last_name]
+    click_button "Sign up"
+    expect(page).to have_content "Logged in as #{valid_attributes[:email]}"
+    click_link("Logout", match: :first)
+    visit "/"
+    fill_in "Email", with: valid_attributes[:email]
+    fill_in "Password", with: valid_attributes[:password] + "wrong"
+    click_button "Log in"
+    expect(page).to_not have_content "Logged in as #{valid_attributes[:email]}"
   end
 end
