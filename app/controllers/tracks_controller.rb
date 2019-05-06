1558a622267dab7f68d18a1829e94d441c840e5f
Marc Magdi <mavloper@gmail.com>
2019-05-06 22:12:33 +0200

    Add search to tracks

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -3,12 +3,20 @@ class TracksController < ApplicationController
     if current_user.tags.empty?
       redirect_to tags_path
     end
-
     @user = current_user
-    userTags = UserTag.where(user_id: @user.id)
-    tag_ids = userTags.select(:tag_id)
-    trackTag = TrackTag.where(tag_id: tag_ids)
-    track_ids = trackTag.select(:track_id)
-    @tracks = Track.where(id: track_ids)
+    tracksFilter = params[:track]
+    @tracks = nil
+
+    if (tracksFilter == nil)
+      userTags = UserTag.where(user_id: @user.id)
+      tag_ids = userTags.select(:tag_id)
+      trackTag = TrackTag.where(tag_id: tag_ids)
+      track_ids = trackTag.select(:track_id)
+      @tracks = Track.where(id: track_ids)
+      @header_title = "Your relevant tracks"
+    else
+      @header_title = "Your search results for: " + tracksFilter
+      @tracks = Track.where("title LIKE :query", query: "%#{tracksFilter}%")
+    end
   end
 end
