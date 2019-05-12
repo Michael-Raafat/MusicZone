080445375b5929d8bccc56fbe906854fe7962740
Michael <micoraafat96@gmail.com>
2019-05-12 13:51:01 +0200

    adding tests for tags

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -55,4 +55,48 @@ RSpec.describe TagsController, type: :controller do
       expect(user.reload.tags).to match_array([new_tags[0], new_tags[1], new_tags[4]])
     end
   end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the :index view" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+
+    it "redirect if not logged in" do
+      sign_out user
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+    it "has all tags in system" do
+      tag = FactoryBot.create(:tag)
+      get :index
+      expect(assigns(:tags)).to match_array([tag])
+    end
+
+    it "tag has tracks" do
+      new_tracks = FactoryBot.create_list(:track, 4)
+      new_tags = FactoryBot.create_list(:tag, 6)
+      TrackTag.create(:track => new_tracks[0], :tag => new_tags[0])
+      TrackTag.create(:track => new_tracks[2], :tag => new_tags[0])
+      TrackTag.create(:track => new_tracks[1], :tag => new_tags[2])
+      get :show, :params => { id: new_tags[0].id }
+      expect(assigns(:tracks)).to match_array([new_tracks[0], new_tracks[2]])
+    end
+
+    it "tag empty tracks" do
+      new_tracks = FactoryBot.create_list(:track, 4)
+      new_tags = FactoryBot.create_list(:tag, 6)
+      TrackTag.create(:track => new_tracks[0], :tag => new_tags[0])
+      TrackTag.create(:track => new_tracks[2], :tag => new_tags[0])
+      TrackTag.create(:track => new_tracks[1], :tag => new_tags[2])
+      get :show, :params => { id: new_tags[1].id }
+      expect(assigns(:tracks)).to match_array([])
+    end
+  end
 end
